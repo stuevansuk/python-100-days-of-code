b5cc7eba32d4bd86fd46c73ffb0fabe0fe991467
Stuart Evans <stuart.evans@fanatics.co.uk>
2022-02-18 14:08:25 +0000

    Continuning Strings

--- day-1/main.py
+++ day-1/main.py
@@ -14,3 +14,9 @@ print("Hello\nMy Name Is\nStuart Evans")
 print("Hello" + "Stuart")
 print("Hello" +  " Stuart")
 print("Hello" + " " + "Stuart")
+
+#Fixing Broken Code 
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the" + ' + ' + "sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and \n")

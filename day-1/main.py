063454a38ed800b6d0aae6ca1649ce4598e2a216
Stuart Evans <stuart.evans@fanatics.co.uk>
2022-02-18 14:52:12 +0000

    Day 1 - Input and Length Calculations

--- day-1/main.py
+++ day-1/main.py
@@ -1,4 +1,5 @@
-#Simnple Printing
+# CMD + / to comment lines
+# #Simnple Printing
 print("Day 1 - Python Printing Function")
 print("The Function is declared like this:")
 
@@ -20,3 +21,18 @@ print("Day 1 - String Manipulation")
 print("String Concatenation is done with the" + ' + ' + "sign.")
 print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and \n")
+
+# #Input Functions
+#input() will get the user input from the console
+#print() will print the word Hello and the user input in one string.
+input("What is you name?\n")
+print("Hello " + input("What is your name?\n"))
+
+# Write a program that prints the number of characters in a user's name.
+# len() will calcuate the length of a string.
+# print() will print the value of the length of the string
+name = input("Hello what is your name?\n")
+print(len(name))
+
+#More efficient way of doing it, nesting the funtions!
+print(len(input("What is your name?\n")))

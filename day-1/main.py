b415fffaa83819ca40b319f89be8a8204c814cd8
Stuart Evans <stuart.evans@fanatics.co.uk>
2022-02-18 12:41:44 +0000

    Strings

--- day-1/main.py
+++ day-1/main.py
@@ -1,4 +1,16 @@
+#Simnple Printing
 print("Day 1 - Python Printing Function")
 print("The Function is declared like this:")
+
+#Printing with "" in Strings
 print("print('what to print')")
 print('print("what to print")')
+
+#Printing on New Lines
+print("Hello World!\nHello World")
+print("Hello\nMy Name Is\nStuart Evans")
+
+#String Concatenation
+print("Hello" + "Stuart")
+print("Hello" +  " Stuart")
+print("Hello" + " " + "Stuart")

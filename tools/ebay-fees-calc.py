773f63ce79d6d41b58780512d6d570b429267a93
Stuart Evans <stuart.evans@fanatics.co.uk>
2022-02-27 20:21:21 +0000

    Slight update to workding

--- tools/ebay-fees-calc.py
+++ tools/ebay-fees-calc.py
@@ -2,7 +2,7 @@ from tkinter import SOLID
 print("\n-------Ebay Fees Calculator---------")
 purchase_price = float(input("Purhcased Price : "))
 sold_price = float(input("Sold Price : "))
-ebay_fees = float(12)
+ebay_fees = float(12)   
 total_fees = sold_price / 100 * ebay_fees
 total_return = round(sold_price - total_fees, 2)
 total_profit = round(sold_price - total_fees - purchase_price,2)
